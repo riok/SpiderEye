3ada2dfeeb14559b064927656e219ba4bcf7158b
Manuel Allenspach <manuel@riok.ch>
2021-01-04 19:41:26 +0100

    feat: add field indicating UI friendly errors

--- Source/SpiderEye.Core/Bridge/Models/ApiResultModel.cs
+++ Source/SpiderEye.Core/Bridge/Models/ApiResultModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using SpiderEye.UI;
 
 namespace SpiderEye.Bridge.Models
 {
@@ -17,6 +18,7 @@ namespace SpiderEye.Bridge.Models
         public string Error { get; set; }
         public string ErrorTypeName { get; set; }
         public string ErrorTypeFullName { get; set; }
+        public bool IsUiFriendlyError { get; set; }
         public string ErrorDetail { get; set; }
 
         [JsonProperty(nameof(Value))]
@@ -34,13 +36,23 @@ namespace SpiderEye.Bridge.Models
 
         public static ApiResultModel FromError(Exception exception)
         {
+            var error = exception.Message;
+            var isUiFriendlyError = false;
+
+            if (exception is IUiFriendlyException uiException)
+            {
+                error = uiException.UiMessage;
+                isUiFriendlyError = true;
+            }
+
             return new ApiResultModel
             {
                 Value = null,
                 Success = false,
-                Error = exception.Message,
+                Error = error,
                 ErrorTypeName = exception.GetType().Name,
                 ErrorTypeFullName = exception.GetType().FullName,
+                IsUiFriendlyError = isUiFriendlyError,
                 ErrorDetail = exception.ToString(),
             };
         }
---
+++ Source/SpiderEye.Core/UI/IUiFriendlyException.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SpiderEye.UI
+{
+    /// <summary>
+    /// Exceptions implementing this interface signal that they have a readable <see cref="UiMessage"/>, which can be shown in the UI (whatever that may be).
+    /// </summary>
+    [SuppressMessage("Naming", "CA1711:Identifiers should not have incorrect suffix", Justification = "This is exception related")]
+    public interface IUiFriendlyException
+    {
+        /// <summary>
+        /// Gets the readable exception message.
+        /// </summary>
+        public string UiMessage { get; }
+    }
+}
