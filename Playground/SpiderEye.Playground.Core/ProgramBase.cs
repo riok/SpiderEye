fca7b0b6bacd9f59ba0b49fafd7ed436d6f88a1e
latonz <lars@riok.ch>
2022-01-25 19:23:20 +0100

    feat: implement local directory mapping for macos

--- Playground/SpiderEye.Playground.Core/ProgramBase.cs
+++ Playground/SpiderEye.Playground.Core/ProgramBase.cs
@@ -6,7 +6,7 @@ namespace SpiderEye.Playground.Core
 {
     public abstract class ProgramBase
     {
-        public static string CustomFileHost { get; private set; }
+        public static Uri CustomFileHost { get; private set; }
 
         private static Window _mainWindow;
         private static ServiceProvider _serviceProvider;
@@ -21,69 +21,67 @@ namespace SpiderEye.Playground.Core
             using var serviceProvider = serviceCollection.BuildServiceProvider();
             _serviceProvider = serviceProvider;
 
-            using (var window = new Window(serviceProvider))
+            using var window = new Window(serviceProvider);
+            _mainWindow = window;
+            window.Title = "SpiderEye Playground";
+            window.UseBrowserTitle = true;
+            window.EnableScriptInterface = true;
+            window.CanResize = true;
+            window.BackgroundColor = "#303030";
+            window.Size = new Size(800, 600);
+            window.MinSize = new Size(300, 200);
+            window.MaxSize = new Size(1200, 900);
+            window.Icon = icon;
+            window.Navigating += (_, uri) => Console.WriteLine("uri changed: " + uri.Url);
+
+            var windowMenu = new Menu();
+            var appMenu = windowMenu.MenuItems.AddLabelItem("App");
+            appMenu.MenuItems.AddMacOsHide();
+            appMenu.MenuItems.AddMacOsHideOtherApplications();
+            appMenu.MenuItems.AddMacOsUnhideAllApplications();
+            appMenu.MenuItems.AddMacOsSeparator();
+
+            var quitMenu = appMenu.MenuItems.AddLabelItem("Quit");
+            quitMenu.SetSystemShortcut(SystemShortcut.Close);
+            quitMenu.Click += (s, e) => Application.Exit();
+
+            windowMenu.MenuItems.AddMacOsEdit();
+            windowMenu.MenuItems.AddMacOsView();
+
+            var mainMenu = windowMenu.MenuItems.AddLabelItem("Main Menu");
+            mainMenu.MenuItems.AddLabelItem("Entry 1");
+            mainMenu.MenuItems.AddSeparatorItem();
+            mainMenu.MenuItems.AddLabelItem("Entry 2");
+            var showModalMenu = mainMenu.MenuItems.AddLabelItem("Show Modal");
+            showModalMenu.Click += (s, e) => ShowModalMenu_Click(true);
+            showModalMenu.SetShortcut(ModifierKey.Control | ModifierKey.Shift, Key.M);
+
+            var showWindowMenu = mainMenu.MenuItems.AddLabelItem("Show Window");
+            showWindowMenu.Click += (s, e) => ShowModalMenu_Click(false);
+            showWindowMenu.SetShortcut(ModifierKey.Control | ModifierKey.Shift, Key.W);
+
+            windowMenu.MenuItems.AddMacOsWindow();
+
+            var helpMenu = windowMenu.MenuItems.AddLabelItem("Help");
+            var helpItem = helpMenu.MenuItems.AddLabelItem("MyHelp");
+            helpItem.SetSystemShortcut(SystemShortcut.Help);
+
+            window.Menu = windowMenu;
+
+            if (window.MacOsOptions != null)
             {
-                _mainWindow = window;
-                window.Title = "SpiderEye Playground";
-                window.UseBrowserTitle = true;
-                window.EnableScriptInterface = true;
-                window.CanResize = true;
-                window.BackgroundColor = "#303030";
-                window.Size = new Size(800, 600);
-                window.MinSize = new Size(300, 200);
-                window.MaxSize = new Size(1200, 900);
-                window.Icon = icon;
-                window.Navigating += (sender, uri) => Console.WriteLine("uri changed: " + uri);
-
-                var windowMenu = new Menu();
-                var appMenu = windowMenu.MenuItems.AddLabelItem("App");
-                appMenu.MenuItems.AddMacOsHide();
-                appMenu.MenuItems.AddMacOsHideOtherApplications();
-                appMenu.MenuItems.AddMacOsUnhideAllApplications();
-                appMenu.MenuItems.AddMacOsSeparator();
-
-                var quitMenu = appMenu.MenuItems.AddLabelItem("Quit");
-                quitMenu.SetSystemShortcut(SystemShortcut.Close);
-                quitMenu.Click += (s, e) => Application.Exit();
-
-                windowMenu.MenuItems.AddMacOsEdit();
-                windowMenu.MenuItems.AddMacOsView();
-
-                var mainMenu = windowMenu.MenuItems.AddLabelItem("Main Menu");
-                mainMenu.MenuItems.AddLabelItem("Entry 1");
-                mainMenu.MenuItems.AddSeparatorItem();
-                mainMenu.MenuItems.AddLabelItem("Entry 2");
-                var showModalMenu = mainMenu.MenuItems.AddLabelItem("Show Modal");
-                showModalMenu.Click += (s, e) => ShowModalMenu_Click(true);
-                showModalMenu.SetShortcut(ModifierKey.Control | ModifierKey.Shift, Key.M);
-
-                var showWindowMenu = mainMenu.MenuItems.AddLabelItem("Show Window");
-                showWindowMenu.Click += (s, e) => ShowModalMenu_Click(false);
-                showWindowMenu.SetShortcut(ModifierKey.Control | ModifierKey.Shift, Key.W);
-
-                windowMenu.MenuItems.AddMacOsWindow();
-
-                var helpMenu = windowMenu.MenuItems.AddLabelItem("Help");
-                var helpItem = helpMenu.MenuItems.AddLabelItem("MyHelp");
-                helpItem.SetSystemShortcut(SystemShortcut.Help);
-
-                window.Menu = windowMenu;
-
-                if (window.MacOsOptions != null)
-                {
-                    window.MacOsOptions.Appearance = MacOsAppearance.DarkAqua;
-                }
-
-                CustomFileHost = window.RegisterLocalDirectoryMapping(".");
-
-                SetDevSettings(window);
-
-                // the port number is defined in the angular.json file (under "architect"->"serve"->"options"->"port")
-                // note that you have to run the angular dev server first (npm run watch)
-                Application.UriWatcher = new AngularDevUriWatcher("http://localhost:65400");
-                Application.ContentProvider = new EmbeddedContentProvider("Angular/dist");
-                Application.Run(window, "/index.html");
+                window.MacOsOptions.Appearance = MacOsAppearance.DarkAqua;
             }
+
+            CustomFileHost = window.RegisterLocalDirectoryMapping(".");
+
+            SetDevSettings(window);
+
+            // the port number is defined in the angular.json file (under "architect"->"serve"->"options"->"port")
+            // note that you have to run the angular dev server first (npm run watch)
+            Application.UriWatcher = new AngularDevUriWatcher("http://localhost:65400");
+            Application.ContentProvider = new EmbeddedContentProvider("Angular/dist");
+            Application.Run(window, "/index.html");
         }
 
         private static void ShowModalMenu_Click(bool modal)
